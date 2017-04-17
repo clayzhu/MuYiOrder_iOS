b4689521dd63dd8aaf5d9c12f3b189d5b2e3bf2b
Clay Zhu <clayzhu@163.com>
2017-04-17 22:13:27 +0800

    建立 Xcode 项目。

---
+++ MuYiOrder/MuYiOrder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MuYiOrder
+//
+//  Created by ug19 on 2017/4/17.
+//  Copyright © 2017年 Ugood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MuYiOrder/MuYiOrder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MuYiOrder
+//
+//  Created by ug19 on 2017/4/17.
+//  Copyright © 2017年 Ugood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
