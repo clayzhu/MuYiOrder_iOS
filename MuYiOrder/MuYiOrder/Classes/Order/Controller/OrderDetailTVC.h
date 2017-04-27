50029288325f0d0232b8e5a5d1d780b326a71d25
Clay Zhu <clayzhu@163.com>
2017-04-27 23:38:08 +0800

    添加订单详情页。

---
+++ MuYiOrder/MuYiOrder/Classes/Order/Controller/OrderDetailTVC.h
@@ -0,0 +1,13 @@
+//
+//  OrderDetailTVC.h
+//  MuYiOrder
+//
+//  Created by ug19 on 2017/4/27.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface OrderDetailTVC : BaseTableViewController
+
+@end
