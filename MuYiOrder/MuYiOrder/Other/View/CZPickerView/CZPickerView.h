58a25f87f8e9e557802ed3904439bae2139f8c40
Clay Zhu <clayzhu@163.com>
2017-05-13 15:26:55 +0800

    创建 CZPickerView.

---
+++ MuYiOrder/MuYiOrder/Other/View/CZPickerView/CZPickerView.h
@@ -0,0 +1,15 @@
+//
+//  CZPickerView.h
+//  MuYiOrder
+//
+//  Created by ug19 on 2017/5/13.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZPickerView : UIView
+
+@property (strong, nonatomic) UIPickerView *pickerView;
+
+@end
---
+++ MuYiOrder/MuYiOrder/Other/View/CZPickerView/CZPickerView.m
@@ -0,0 +1,29 @@
+//
+//  CZPickerView.m
+//  MuYiOrder
+//
+//  Created by ug19 on 2017/5/13.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import "CZPickerView.h"
+
+@interface CZPickerView () <UIPickerViewDataSource, UIPickerViewDelegate>
+
+@end
+
+@implementation CZPickerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+#pragma mark - UIPickerViewDataSource
+
+#pragma mark - UIPickerViewDelegate
+
+@end
