0011433e42ab858929f889b19bbc09a3e4ef0234
Clay Zhu <clayzhu@163.com>
2017-05-14 00:06:26 +0800

    CZPickerView 添加属性。

--- MuYiOrder/MuYiOrder/Other/View/CZPickerView/CZPickerView.h
+++ MuYiOrder/MuYiOrder/Other/View/CZPickerView/CZPickerView.h
@@ -11,5 +11,8 @@
 @interface CZPickerView : UIView
 
 @property (strong, nonatomic) UIPickerView *pickerView;
+@property (strong, nonatomic) NSArray<NSString *> *dataSource;
+@property (assign, nonatomic) NSUInteger selectedRow;
+@property (strong, nonatomic) NSString *identifier;
 
 @end
