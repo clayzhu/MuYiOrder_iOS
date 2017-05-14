cb78d6ad68b3cc9b3625f7323cc730812b5401f2
Clay Zhu <clayzhu@163.com>
2017-05-14 16:17:53 +0800

    添加注释。

--- MuYiOrder/MuYiOrder/Other/View/CZPickerView/CZPickerView.h
+++ MuYiOrder/MuYiOrder/Other/View/CZPickerView/CZPickerView.h
@@ -12,18 +12,26 @@
 
 @protocol CZPickerViewDelegate <NSObject>
 
+/**
+ 点击“取消”或“确定”按钮后回调的代理方法
+
+ @param czPickerView CZPickerView 对象，可以通过该对象获取 identifier 等属性的值
+ @param index 选择的行数
+ @param isClickSure YES-点击了“确定”，NO-点击了“取消”
+ */
 - (void)czPickerView:(CZPickerView *)czPickerView selectedRow:(NSUInteger)index clickSureButton:(BOOL)isClickSure;
 
 @end
 
 @interface CZPickerView : UIView
-
+/** 如果一个页面需要使用多个 CZPickerView，可以使用 identifier 标记 */
 @property (strong, nonatomic) NSString *identifier;
 
 @property (strong, nonatomic) UIPickerView *pickerView;
 @property (strong, nonatomic) NSArray<NSString *> *dataSource;
 @property (assign, nonatomic) NSUInteger selectedRow;
 
+/** 设定主色调 */
 @property (strong, nonatomic) UIColor *mainColor;
 
 @property (weak, nonatomic) id<CZPickerViewDelegate> delegate;
