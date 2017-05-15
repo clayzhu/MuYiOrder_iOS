039edf5450deda6fc46577a99a54d438740b87e0
Clay Zhu <clayzhu@163.com>
2017-05-15 23:05:43 +0800

    创建 CZImagePickerView.

---
+++ MuYiOrder/MuYiOrder/Other/View/CZImagePickerView/CZImagePickerView.h
@@ -0,0 +1,13 @@
+//
+//  CZImagePickerView.h
+//  MuYiOrder
+//
+//  Created by ug19 on 2017/5/15.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZImagePickerView : UIView
+
+@end
---
+++ MuYiOrder/MuYiOrder/Other/View/CZImagePickerView/CZImagePickerView.m
@@ -0,0 +1,21 @@
+//
+//  CZImagePickerView.m
+//  MuYiOrder
+//
+//  Created by ug19 on 2017/5/15.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import "CZImagePickerView.h"
+
+@implementation CZImagePickerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
